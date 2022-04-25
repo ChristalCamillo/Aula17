d6805e3d38b675e58c6be445514ee96aa36fbd3b
christal.camillo <christal.b.camillo@gmail.com>
2022-04-25 16:56:35 -0300

    falta compreender o retorno

---
+++ src/Funcionario.kt
@@ -0,0 +1,28 @@
+//1. Crie uma classe Funcionario que obedeça à descrição abaixo:
+//   1. A classe possui os atributos nome, sobrenome, horasTrabalhadas e valorPorHora
+//
+//   2. O método nomeCompleto deve escrever na tela o atributo nome concatenado ao atributo sobrenome.
+//
+//   3. O método calcularSalario faz o cálculo de quanto o funcionário irá receber no mês, multiplicando o atributo horasTrabalhadas pelo atributo valorPorHora. Em seguida, escreve o valor na tela.
+//
+//   4. O método incrementarHoras adiciona um valor passado por parâmetro ao valor já existente no atributo valorPorHora
+//
+class Funcionario(var horasTrabalhadas: Double, var valorPorHora: Double) {
+    var nome: String = ""
+    var sobrenome: String = ""
+
+    fun nomeCompleto() {
+        println("O nome é $nome e o sobrenome é $sobrenome")
+    }
+
+    fun calcularSalario(horasTrabalhadas: Double, valorPorHora: Double){
+        var salarioMensal = horasTrabalhadas*valorPorHora
+        println("O salário mensal é $salarioMensal")
+    }
+
+    fun incrementarHoras(incrementarHoras: Double): Double{
+        var horaIncrementada = valorPorHora+incrementarHoras
+        return horaIncrementada
+    }
+
+}
---
+++ src/Principal.kt
@@ -0,0 +1,26 @@
+//Crie o método main e instancie a classe Funcionário criada, criando um objeto novoFuncionario do tipo Funcionario:
+//
+//
+//   * Atribua o valor “Luis” ao atributo nome.
+//   * Atribua o valor “Silva” ao atributo sobrenome.
+//   *  Atribua o valor 10 ao atributo horasTrabalhadas
+//   *  Atribua o valor 25.50 ao atributo valorPorHora.
+//   * Chame o método nomeCompleto.
+//   * Chame o método calcularSalario.
+//   * Adicione 8 ao atributo horasTrabalhadas utilizando o método incrementarHoras.
+//   * Chame novamente o método calcularSalario.
+//   * Crie um ArrayList e instancie outros 9 funcionários com diferentes atributos a esse ArrayList
+//   * Exiba os valores do ArrayList criado
+
+fun main() {
+    var novoFuncionario: Funcionario = Funcionario(10.00, 25.50)
+
+    novoFuncionario.nome = "Luis"
+    novoFuncionario.sobrenome = "Silva"
+
+    novoFuncionario.nomeCompleto()
+    novoFuncionario.calcularSalario(10.00, 25.50)
+    novoFuncionario.incrementarHoras(8.00)
+    novoFuncionario.calcularSalario(horasTrabalhadas = horaIncrementada, )
+
+}
